cca3c72eda35f0d86ee7e289b697853f34cf85ef
Yifang Zuo <97566388+YvonneZuo@users.noreply.github.com>
2023-04-20 19:02:53 +0000

    tests for cart-dish-cartController

---
+++ src/test/java/edu/northeastern/cs5500/starterbot/controller/CartControllerTest.java
@@ -0,0 +1,81 @@
+package edu.northeastern.cs5500.starterbot.controller;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import edu.northeastern.cs5500.starterbot.model.Cart;
+import edu.northeastern.cs5500.starterbot.model.Dish;
+import edu.northeastern.cs5500.starterbot.repository.InMemoryRepository;
+import org.junit.jupiter.api.Test;
+
+class CartControllerTest {
+    static final String USER_ID_1 = "28484913146489";
+
+    private CartController getCartController() {
+        return new CartController(new InMemoryRepository<>());
+    }
+
+    @Test
+    void testGetItemsInCart() {
+        CartController cartController = getCartController();
+        // test empty cart
+        Dish dish = new Dish("rice", 0.99);
+        assertThat(cartController.getItemsInCart(USER_ID_1)).doesNotContainKey(dish);
+
+        // test non-empty cart
+        cartController.addToCart(dish, USER_ID_1);
+        cartController.addToCart(dish, USER_ID_1);
+        assertThat(cartController.getItemsInCart(USER_ID_1)).containsKey(dish);
+    }
+
+    @Test
+    void testAddToCart() {
+        // emtpy cart
+        CartController cartController = getCartController();
+        Dish dish = new Dish("rice", 0.99);
+        assertThat(cartController.getItemsInCart(USER_ID_1)).isEmpty();
+        // add to cart
+        cartController.addToCart(dish, USER_ID_1);
+        // non-empty cart
+        assertThat(cartController.getItemsInCart(USER_ID_1)).containsKey(dish);
+    }
+
+    @Test
+    void testRemoveFromCart() {
+        // add to cart
+        CartController cartController = getCartController();
+        Dish dish = new Dish("rice", 0.99);
+        cartController.addToCart(dish, USER_ID_1);
+        assertThat(cartController.getItemsInCart(USER_ID_1)).containsKey(dish);
+        // remove from cart
+        cartController.removeFromCart(dish, USER_ID_1);
+        // cart after removal
+        assertThat(cartController.getItemsInCart(USER_ID_1)).doesNotContainKey(dish);
+    }
+
+    @Test
+    void testGetCartForUser() {
+        // test get empty cart for user
+        CartController cartController = getCartController();
+        Cart cart = cartController.getCartForUser(USER_ID_1);
+        assertThat(cart.getItems()).isEmpty();
+        assertThat(cart.getDiscordUserId()).isEqualTo(USER_ID_1);
+
+        // test get non-empty cart for user
+        Dish dish = new Dish("rice", 0.99);
+        cartController.addToCart(dish, USER_ID_1);
+        assertThat(cart.getItems()).isNotEmpty();
+    }
+
+    @Test
+    void testClearCart() {
+        // add to cart
+        CartController cartController = getCartController();
+        Dish dish = new Dish("rice", 0.99);
+        cartController.addToCart(dish, USER_ID_1);
+        assertThat(cartController.getItemsInCart(USER_ID_1)).containsKey(dish);
+        // clear cart
+        cartController.clearCart(USER_ID_1);
+        // empty cart
+        assertThat(cartController.getItemsInCart(USER_ID_1)).isEmpty();
+    }
+}
---
+++ src/test/java/edu/northeastern/cs5500/starterbot/model/DishTest.java
@@ -0,0 +1,21 @@
+/**
+ * This contains JUnit tests for the Dish class. Since the Dish class is using @Data noatation, we
+ * do not test getters, setters and equality.
+ */
+package edu.northeastern.cs5500.starterbot.model;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class DishTest {
+    private static final String DISH_NAME = "Rice";
+    private static final double PRICE = 1.5;
+
+    @Test
+    void testDishConstrucotr() {
+        Dish dish = new Dish(DISH_NAME, PRICE);
+        assertThat(dish.getDishName()).isEqualTo(DISH_NAME);
+        assertThat(dish.getPrice()).isEqualTo(PRICE);
+    }
+}
