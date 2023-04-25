58047fe2ce90e1d8ede5b5580d723e61837b446e
Yue Zhao <105524311+yuezhaoo@users.noreply.github.com>
2023-04-25 06:40:22 +0000

    Replace storeId with dishes

--- src/main/java/edu/northeastern/cs5500/starterbot/model/Receipt.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Receipt.java
@@ -1,5 +1,6 @@
 package edu.northeastern.cs5500.starterbot.model;
 
+import java.util.Map;
 import javax.annotation.Nonnull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,5 +16,5 @@ public class Receipt implements Model {
     ObjectId id;
 
     @Nonnull private ObjectId userId;
-    @Nonnull private ObjectId storeId;
+    @Nonnull private Map<Dish, Integer> dishes;
 }
