912fc9a8920c110bf7c69b8e05941c80a55ec56c
Yue Zhao <105524311+yuezhaoo@users.noreply.github.com>
2023-04-27 06:58:33 +0000

    Refactor MenuCommand

--- src/main/java/edu/northeastern/cs5500/starterbot/model/Menu.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Menu.java
@@ -5,13 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Menu {
-    ObjectId id;
+    int id;
     Set<MenuItem> menuItems;
 }
